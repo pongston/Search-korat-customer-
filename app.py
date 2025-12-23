b71828359e7286f9573976b83bb791fe882d8a4b
pongston <59470949+pongston@users.noreply.github.com>
2025-12-23 09:09:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, request, jsonify, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
 import sqlite3
+import os
+
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+app.secret_key = "pongston_secret"
 
 app = Flask(__name__)
 app.secret_key = "pongston_secret"
